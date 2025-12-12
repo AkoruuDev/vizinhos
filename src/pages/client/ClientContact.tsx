4cafb13443501b6f32624ce5bfdd00e9069925c5
Akoruu Dev <akoruu.dev@gmail.com>
2025-12-12 17:50:12 -0300

    [fix]: fixed tailwindcss importation

--- src/pages/client/ClientContact.tsx
+++ src/pages/client/ClientContact.tsx
@@ -1,6 +1,6 @@
 function ClientContact() {
     return (
-        <></>
+        <>ClientContact</>
     )
 }
 
--- src/pages/platform/Plans.tsx
+++ src/pages/platform/Plans.tsx
@@ -1,6 +1,6 @@
 function Plans() {
     return (
-        <></>
+        <>Plans</>
     )
 }
 
