9de24bb7c8721a3757eb47cf3ea3b50669461378
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-07 21:14:20 +0000

    [feat]: update Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,20 @@
+import Menu from "./Menu";
+import MainSpacer from "./spacers/MainSpacer";
+
 function Footer() {
     return (
-        <></>
+        <footer className="flex justify-center w-full mt-16 p-4 bg-neutral-200">
+            <div className="flex flex-col items-center w-full">
+                <div>
+                    <Menu />
+                </div>
+                <MainSpacer />
+                <div className="text-center">
+                    <p>Todos os direitos reservados | Grupo Majas 2025</p>
+                    <p>Desenvolvido por <a href="#">Allan Majas</a></p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
