aebe7e8e7250ea5d85154bbf12ec26e276532f59
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-07 21:12:42 +0000

    [feat]: create menu component

---
+++ src/components/layout/Menu.tsx
@@ -0,0 +1,13 @@
+function Menu() {
+    return (
+        <nav>
+            <ul className="flex gap-4 mx-5">
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Planos</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu
