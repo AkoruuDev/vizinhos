1f79346299a38301461ccad4ed8ae6f220470e23
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-07 21:13:17 +0000

    [feat]: create MainSpacer spacer for project

---
+++ src/components/layout/spacers/MainSpacer.tsx
@@ -0,0 +1,11 @@
+function MainSpacer() {
+  return (
+    <div className="flex items-center w-full my-12">
+      <div className="flex-grow h-px bg-neutral-800" />
+      <div className="mx-4 w-3 h-3 bg-neutral-800 rotate-45" />
+      <div className="flex-grow h-px bg-neutral-800" />
+    </div>
+  )
+}
+
+export default MainSpacer
