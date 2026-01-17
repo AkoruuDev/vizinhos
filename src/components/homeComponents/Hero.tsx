83953aa0fd0f3cca8e3bdf309871b128f56f13d0
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-17 02:28:23 +0000

    style: update basic style from Hero

--- src/components/homeComponents/Hero.tsx
+++ src/components/homeComponents/Hero.tsx
@@ -17,7 +17,7 @@ function Hero() {
     }, [])
 
     return (
-        <section>
+        <section className="flex flex-col items-center relative">
             <ImageComponent image={activeImages[currentIndex]} />
             <HeroDots 
                 total={activeImages.length}
@@ -32,10 +32,10 @@ export default Hero;
 
 function ImageComponent({image} : {image: Image}) {
     const picture = (
-        <picture>
+        <picture className="w-full">
             <source media="(min-width: 1024px)" srcSet={image.src.wide} />
             <source media="(min-width: 640px)" srcSet={image.src.landscape} />
-            <img src={image.src.square} alt={image.alt} />
+            <img src={image.src.square} alt={image.alt} className="w-full h-auto" />
         </picture>
     )
 
@@ -52,13 +52,13 @@ function ImageComponent({image} : {image: Image}) {
 
 function HeroDots({total, currentIndex, onSelect} : {total: number, currentIndex: number, onSelect: (index: number) => void}) {
     return (
-        <div>
+        <div className="absolute bottom-3">
             {Array.from({ length: total }).map((_, index) => (
                 <button
                     key={index}
                     onClick={() => onSelect(index)}
                     aria-label={`Hero ${index + 1}`}
-                    className={`size-4 m-2 rounded-full border-2 border-indigo-400 ${index === currentIndex ? 'bg-indigo-400' : 'bg-transparent' }`}
+                    className={`size-4 m-2 rounded-full border-2 border-indigo-400 ${index === currentIndex ? 'bg-indigo-400' : 'bg-transparent' } ${index === currentIndex ? 'w-8' : 'bg-transparent' } ease-linear duration-200`}
                 />
             ))}
         </div>
