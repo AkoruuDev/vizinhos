0c8c095fc601ed155cbdc5280fa528b226be3ac2
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-17 02:27:42 +0000

    feat: import images to images component

--- src/components/homeComponents/HeroImages.tsx
+++ src/components/homeComponents/HeroImages.tsx
@@ -1,3 +1,15 @@
+import hero1Square from "@/assets/Heros/hero1-square.png";
+import hero1Landscape from "@/assets/Heros/hero1-landscape.png";
+import hero1Wide from "@/assets/Heros/hero1-wide.png";
+
+import hero2Square from "@/assets/Heros/hero2-square.png";
+import hero2Landscape from "@/assets/Heros/hero2-landscape.png";
+import hero2Wide from "@/assets/Heros/hero2-wide.png";
+
+import hero3Square from "@/assets/Heros/hero3-square.png";
+import hero3Landscape from "@/assets/Heros/hero3-landscape.png";
+import hero3Wide from "@/assets/Heros/hero3-wide.png";
+
 interface Image {
     id: string,
     src: {
@@ -14,29 +26,29 @@ const images: Image[] = [
     {
         id: '0o1',
         src: {
-            square: "",
-            landscape: "",
-            wide: ""
+            square: hero1Square,
+            landscape: hero1Landscape,
+            wide: hero1Wide
         },
-        alt: "",
+        alt: "Hero 1",
         isActive: true
     },{
-        id: '0o1',
+        id: '0o2',
         src: {
-            square: "",
-            landscape: "",
-            wide: ""
+            square: hero2Square,
+            landscape: hero2Landscape,
+            wide: hero2Wide
         },
-        alt: "",
+        alt: "Hero 2",
         isActive: true
     },{
-        id: '0o1',
+        id: '0o3',
         src: {
-            square: "",
-            landscape: "",
-            wide: ""
+            square: hero3Square,
+            landscape: hero3Landscape,
+            wide: hero3Wide
         },
-        alt: "",
+        alt: "Hero 3",
         isActive: true
     }
 ]
