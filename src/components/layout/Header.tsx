f21b8d311c879b47e705811a78ed8bd2863bba37
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-01-21 02:08:16 +0000

    feat: update Header style

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,12 +6,12 @@ function Header() {
     return (
         <header className="w-full flex justify-center bg-neutral-200">
             <div className="w-[80%] flex justify-between items-center">
-                <img src={icon} alt="logo-icon" className="h-12" />
-                <div className="min-[431px]:hidden flex items-center">
-                    <button className="mr-5 py-8 px-6 rounded-5 ">Entrar</button>
+                <img src={icon} alt="logo-icon" className="h-18" />
+                <div className="min-[531px]:hidden flex items-center">
+                    <button className="mx-2 my-2 py-3 px-6 rounded-3xl border-4 bg-indigo-400 border-indigo-400 font-bold text-neutral-200 hover:bg-neutral-200 hover:text-indigo-400 hover:cursor-pointer ease-linear duration-200">Entrar</button>
                     <img src={menu} alt="menu-ico" className="h-12" />
                 </div>
-                <div className="max-[431px]:hidden flex items-center">
+                <div className="max-[531px]:hidden flex items-center">
                     <Menu />
                     <button className="mx-2 my-2 py-3 px-6 rounded-3xl border-4 bg-indigo-400 border-indigo-400 font-bold text-neutral-200 hover:bg-neutral-200 hover:text-indigo-400 hover:cursor-pointer ease-linear duration-200">Entrar</button>
                 </div>
