a193d9299df1e4afa9422e7498c74c6642916c58
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2026-02-06 17:19:35 +0000

    fetch: coding the login component

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -1,28 +1,51 @@
+import { useState, type Dispatch, type SetStateAction } from "react";
+
 function Login() {
+    const [status, setStatus] = useState("login");
+
     return (
         <>
-            <SignIn />
-            <CreateAccount />
-            <ResetPassword />
+            {status === "login" && <SignIn setStatus={setStatus} />}
+            {status === "create" && <CreateAccount setStatus={setStatus} />}
+            {status === "reset" && <ResetPassword setStatus={setStatus} />}
         </>
     )
 }
 
 export default Login
 
-function CreateAccount() {
+function CreateAccount(setStatus: Dispatch<SetStateAction<string>>) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
     return (
-        <></>
+        <>
+            <section>
+                <form action="submit">
+                    <input id="email" type="text"
+                    value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <label htmlFor="email">email</label>
+                    <input id="password" type="password"
+                    value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor="password">senha</label>
+                </form>
+                <p onClick={() => setStatus("reset")}>Esqueci minha senha</p>
+                {/* adicionar o acesso via OAuth aqui */}
+                <div>
+                    <p>Não tem cadastro?</p>
+                    <p onClick={() => setStatus("create")}>Cadastre-se aqui</p>
+                </div>
+            </section>
+        </>
     );
 }
 
-function SignIn() {
+function SignIn(setStatus) {
     return(
         <></>
     );
 }
 
-function ResetPassword() {
+function ResetPassword(setStatus) {
     return(
         <></>
     );
